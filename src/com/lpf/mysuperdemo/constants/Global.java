2d5ef35a34661c27e1bfa88367a71829f76f18f4
lpftobetheone@163.com <lpftobetheone@163.com>
2015-06-04 15:52:35 +0800

    添加存放变量类

---
+++ src/com/lpf/mysuperdemo/constants/Global.java
@@ -0,0 +1,34 @@
+/**
+ *@Copyright:Copyright (c) 2008 - 2100
+ *@Company:SJS
+ */
+package com.lpf.mysuperdemo.constants;
+
+
+/**
+ *@Title:
+ *@Description:
+ *@Author:liupf5
+ *@Since:2015-6-4
+ *@Version:1.1.0
+ */
+public class Global {
+
+	public static final String SERVER = "http://192.168.191.1:8080/";
+	
+	//轮播图图片
+	
+	public static final String[] mAutoPlayUrl = {
+		SERVER +"jsonProject/images/item01.jpg",
+		SERVER +"jsonProject/images/item02.jpg",
+		SERVER +"jsonProject/images/item01.jpg"};
+	
+	//水平滚动图片
+	
+	public static final String[] mHorizontalListViewUrl = {
+		SERVER +"jsonProject/images/starproduct1.jpg",
+		SERVER +"jsonProject/images/starproduct2.jpg",
+		SERVER +"jsonProject/images/starproduct3.jpg",
+		SERVER +"jsonProject/images/starproduct4.jpg",
+		SERVER +"jsonProject/images/starproduct5.jpg"};
+}
